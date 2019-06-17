f7470c3998f815b9dd9e7dc38010435d1e482e1f
mac <lulili@guardle.com>
2019-06-17 14:11:35 +0800

    map

--- js/map实现.js
+++ js/map实现.js
@@ -1,8 +1,14 @@
 function myMap(fn,context) {
     let arr=Array.prototype.slice.call(context)  //将具有length属性的对象转换成数组
     let mapArray=[];
-
     for (let i=0;i<arr.length;i++){
-     if(!arr.hasOwnProperty(i)) continue
+     if(!arr.hasOwnProperty(i)) continue;
+        mapArray.push(fn.call(context,arr[i],i,this))
     }
+    return mapArray
 }
+
+let arr=[2,4,5]
+
+let res=myMap(item=>item+2,arr);
+console.log(res);
